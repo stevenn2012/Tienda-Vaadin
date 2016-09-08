56648c7affec8cb1fc6119f71b3c9d69882c542c
Steven Puerto <2012stevenn@gmail.com>
2016-09-07 19:08:10 -0500

    cm

--- Framework-Ancho-Fijo-Anotaciones/src/main/java/co/edu/usa/adf/Framework_Ancho_Fijo_Anotaciones/App.java
+++ Framework-Ancho-Fijo-Anotaciones/src/main/java/co/edu/usa/adf/Framework_Ancho_Fijo_Anotaciones/App.java
@@ -3,16 +3,25 @@ package co.edu.usa.adf.Framework_Ancho_Fijo_Anotaciones;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.text.ParseException;
+import java.util.ArrayList;
 
 public class App {
+	
+
+	private static String[] rutasDescriptores = {
+			"Datos/Descriptores/Descriptor_Contacto.txt",
+			"Datos/Descriptores/Descriptor_Evento.txt",
+			"Datos/Descriptores/Descriptor_Contacto2.txt",
+			"Datos/Descriptores/Descriptor_Pais.txt",
+			"Datos/Descriptores/Descriptor_Pelicula.txt",
+			"Datos/Descriptores/Descriptor_Series.txt"
+	};
+	
 	public static void main(String[] args) {
 		try {
-			correrTomate("Datos/Descriptores/Descriptor_Contacto.txt");
-			correrTomate("Datos/Descriptores/Descriptor_Evento.txt");
-			correrTomate("Datos/Descriptores/Descriptor_Contacto2.txt");
-			correrTomate("Datos/Descriptores/Descriptor_Pais.txt");
-			correrTomate("Datos/Descriptores/Descriptor_Pelicula.txt");
-			correrTomate("Datos/Descriptores/Descriptor_Series.txt");
+			for (int i = 0; i < rutasDescriptores.length; i++) {
+				correrFramework(rutasDescriptores[i]);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
@@ -34,11 +43,15 @@ public class App {
 		}
 	}
 	
-	private static void correrTomate(String descriptor) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException, IOException, ParseException {
-		System.out.println("Tomate Descriptor: "+descriptor.split("/")[descriptor.split("/").length-1]);
+	private static void correrFramework(String descriptor) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException, IOException, ParseException {
+		System.out.println("Inicio------------------------------------------------------------------>");
 		franfia prueba = new franfia(descriptor);
-		prueba.leerArchivo();
+		ArrayList<Object> datos = prueba.leerArchivo();
+		for (int i = 0; i < datos.size(); i++) {
+			System.out.println(datos.get(i));
+		}
+		System.out.println("Guardando--------------------------------------------------------------->");
 		prueba.escribirArchivo();
-		System.out.println("-----------------------------------------------");
+		System.out.println("------------------------------------------------------------------------>\n");
 	}
 }
